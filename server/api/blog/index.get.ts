4b17778eb4ab62254adbbb4db48629b43ed12262
Elias Carlston <elias@eliasjames.com>
2026-07-09 11:55:35 -0400

    server support for future publish dates

--- server/api/blog/index.get.ts
+++ server/api/blog/index.get.ts
@@ -1,5 +1,6 @@
 export default eventHandler(async (event) => {
   return event.context.prisma.blog.findMany({
+    where: { OR: [{ publishedAt: null }, { publishedAt: { lte: new Date() } }] },
     select: { id: true, title: true, author: true, createdAt: true },
     orderBy: { createdAt: 'desc' },
   })
--- server/trpc/routers/blog.ts
+++ server/trpc/routers/blog.ts
@@ -3,6 +3,7 @@ import { publicProcedure, router } from '../trpc'
 export const blogRouter = router({
   list: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.blog.findMany({
+      where: { OR: [{ publishedAt: null }, { publishedAt: { lte: new Date() } }] },
       orderBy: { createdAt: 'desc' },
     })
   }),
